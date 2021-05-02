c15fc9f81ee4d5827c4c871fe31dd88db1cc28ae
Lucas Garron <code@garron.net>
2021-05-02 01:57:39 -0700

    FREEZE.unindexed

--- src/demo/alg.cubing.net/index.ts
+++ src/demo/alg.cubing.net/index.ts
@@ -1,5 +1,4 @@
 import "../../cubing/twisty";
 import { useNewFaceNames } from "../../cubing/puzzle-geometry";
-import { Alg } from "../../cubing/alg";
 
-console.log(useNewFaceNames, Alg)
+console.log(useNewFaceNames)
