5f7a5361a3fef6312015c1931eb4ebc4bbe2b987
Lucas Garron <code@garron.net>
2021-05-02 02:26:03 -0700

    FREEZE.unindexed

--- src/cubing/alg/index.ts
+++ src/cubing/alg/index.ts
@@ -1,16 +1,1 @@
 export { Alg } from "./Alg";
-export { AlgBuilder } from "./AlgBuilder";
-export { TraversalDownUp, TraversalUp } from "./traversal";
-export { Example } from "./example";
-export { keyToMove } from "./keyboard";
-export * from "./units";
-
-export { algCubingNetLink, AlgCubingNetOptions } from "./url";
-
-export { experimentalAppendMove } from "./operation";
-export { experimentalIs } from "./is";
-export {
-  directedGenerator as experimentalDirectedGenerator,
-  direct as experimentalDirect,
-  IterationDirection as ExperimentalIterationDirection,
-} from "./iteration";
