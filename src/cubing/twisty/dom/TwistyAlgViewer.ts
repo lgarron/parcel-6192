0e4fe1b0fb582ec31885324c20b3703d7238887c
Lucas Garron <code@garron.net>
2021-05-02 03:00:31 -0700

    FREEZE.unindexed

--- src/cubing/twisty/dom/TwistyAlgViewer.ts
+++ src/cubing/twisty/dom/TwistyAlgViewer.ts
@@ -1,6 +1,8 @@
 import type { Parsed } from "../../alg/parse";
 import { puzzles } from "../../puzzles";
 import { TwistyPlayer } from "../../twisty";
+import { KPuzzleWrapper } from "../3D/puzzles/KPuzzleWrapper";
+import { TreeAlgIndexer } from "../animation/indexer/tree/TreeAlgIndexer";
 
 export class ExperimentalTwistyAlgViewer extends HTMLElement {
   highlighter: MoveHighlighter = new MoveHighlighter();
@@ -12,6 +14,70 @@ export class ExperimentalTwistyAlgViewer extends HTMLElement {
     if (options?.twistyPlayer) {
       this.setTwistyPlayer(options?.twistyPlayer);
     }
+  }
+
+  protected connectedCallback(): void {
+    // nothing to do?
+  }
+
+  private setAlg(alg: Alg): void {
+    this.#domTree = algToDOMTree(alg, {
+      earliestMoveIndex: 0,
+      twistyAlgViewer: this,
+      direction: ExperimentalIterationDirection.Forwards,
+    }).element;
+    this.textContent = "";
+    this.appendChild(this.#domTree);
+  }
+
+  setTwistyPlayer(twistyPlayer: TwistyPlayer): void {
+    if (this.twistyPlayer) {
+      console.warn("twisty-player reassignment is not supported");
+      return;
+    }
+    this.twistyPlayer = twistyPlayer;
+    const sourceAlg = this.twistyPlayer.alg;
+    // TODO: Use proper architecture instead of a heuristic to ensure we have a parsed alg annotated with char indices.
+    const parsedAlg =
+      "charIndex" in (sourceAlg as Partial<Parsed<Alg>>)
+        ? sourceAlg
+        : Alg.fromString(sourceAlg.toString());
+    this.setAlg(parsedAlg);
+    (async () => {
+      const wrapper = new KPuzzleWrapper(
+        await puzzles[twistyPlayer!.puzzle].def(),
+      );
+      const indexer = new TreeAlgIndexer(wrapper, parsedAlg);
+      twistyPlayer.timeline.addTimestampListener({
+        onTimelineTimestampChange: (timestamp: MillisecondTimestamp): void => {
+          // TODO: improve perf, e.g. only get notified when the move index changes.
+          this.highlighter.set(
+            indexer.getMove(
+              indexer.timestampToIndex(timestamp),
+            ) as Parsed<Move> | null,
+          );
+        },
+        onTimeRangeChange(_timeRange: TimeRange): void {},
+      });
+    })();
+    twistyPlayer.timeline.addTimestampListener({
+      onTimelineTimestampChange: (timestamp: MillisecondTimestamp) => {
+        if (timestamp !== this.lastClickTimestamp) {
+          this.lastClickTimestamp = null;
+        }
+        const index =
+          this.twistyPlayer?.cursor?.experimentalIndexFromTimestamp(
+            timestamp,
+          ) ?? null;
+        if (index !== null) {
+          // console.log(index);
+          // console.log(this.#domTree.pathToIndex(index));
+        }
+      },
+      onTimeRangeChange: (_timeRange: TimeRange) => {
+        // TODO
+      },
+    });
   }
 
 }
