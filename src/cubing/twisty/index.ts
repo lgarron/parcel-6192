6724957846e5ab27a9f55d142bb22e63575e56c1
Lucas Garron <code@garron.net>
2021-05-02 02:43:06 -0700

    FREEZE.unindexed

--- src/cubing/twisty/index.ts
+++ src/cubing/twisty/index.ts
@@ -1,7 +1,2 @@
-export { Twisty3DPuzzle } from "./3D/puzzles/Twisty3DPuzzle";
-export {
-  experimentalSetShareAllNewRenderers,
-  experimentalShowRenderStats,
-  Twisty3DCanvas,
-} from "./dom/viewers/Twisty3DCanvas";
 export { TwistyPlayer } from "./dom/TwistyPlayer";
+export { ExperimentalTwistyAlgViewer } from "./dom/TwistyAlgViewer";
