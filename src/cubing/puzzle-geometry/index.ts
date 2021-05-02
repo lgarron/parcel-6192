0f74d6f9558f39645cac6b3571a2c42c8615b3f8
Lucas Garron <code@garron.net>
2021-05-02 04:11:52 -0700

    FREEZE.unindexed

--- src/cubing/puzzle-geometry/index.ts
+++ src/cubing/puzzle-geometry/index.ts
@@ -1,23 +1,3 @@
-export { Perm } from "./Perm";
 export {
-  Orbit,
-  OrbitDef,
-  OrbitsDef,
-  Transformation,
-  VisibleState,
-} from "./PermOriSet";
-export {
-  getpuzzle,
-  getPuzzleGeometryByDesc,
   getPuzzleGeometryByName,
-  getpuzzles,
-  parsedesc,
-  PuzzleGeometry,
-  StickerDat,
-  StickerDatAxis,
-  StickerDatFace,
-  StickerDatSticker,
-  useNewFaceNames,
 } from "./PuzzleGeometry";
-export { Quat } from "./Quat";
-export { schreierSims } from "./SchreierSims";
