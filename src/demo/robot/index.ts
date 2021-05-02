10670763fe80cb06f7096c83e6a26cde367c3714
Lucas Garron <code@garron.net>
2021-05-02 01:56:10 -0700

    FREEZE.unindexed

--- src/demo/robot/index.ts
+++ src/demo/robot/index.ts
@@ -1,13 +1,2 @@
-import {
-  connectSmartPuzzle,
-  debugKeyboardConnect,
-} from "../../cubing/bluetooth";
-import { connectSmartRobot } from "../../cubing/bluetooth/smart-robot";
 import "../../cubing/twisty";
 
-
-console.log(
-  connectSmartPuzzle,
-  debugKeyboardConnect,
-  connectSmartRobot,
-)
