6dd163e6ecc72905ff25086191d5179e9cfd2777
Lucas Garron <code@garron.net>
2021-05-01 21:42:48 -0700

    FREEZE.unindexed

--- src/demo/alg.cubing.net/app.ts
+++ src/demo/alg.cubing.net/app.ts
@@ -1,12 +1,8 @@
-import { Alg } from "../../cubing/alg";
-import { puzzles } from "../../cubing/puzzles";
 import {
   TwistyPlayer,
 } from "../../cubing/twisty";
 
 console.log(
-  Alg,
-  puzzles,
   TwistyPlayer
 )
 
