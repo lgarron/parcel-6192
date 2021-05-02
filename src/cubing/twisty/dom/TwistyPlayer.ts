17e4cd72b07104716ffd88b9097b713443a7bcc7
Lucas Garron <code@garron.net>
2021-05-02 02:35:28 -0700

    FREEZE.unindexed

--- src/cubing/twisty/dom/TwistyPlayer.ts
+++ src/cubing/twisty/dom/TwistyPlayer.ts
@@ -1,8 +1,7 @@
-import { ManagedCustomElement } from "./element/ManagedCustomElement";
 import { customElementsShim } from "./element/node-custom-element-shims";
 
 // <twisty-player>
-export class TwistyPlayer extends ManagedCustomElement {
+export class TwistyPlayer extends HTMLElement {
 }
 
 customElementsShim.define("twisty-player", TwistyPlayer);
