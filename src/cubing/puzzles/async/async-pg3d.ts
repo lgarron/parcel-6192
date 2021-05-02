8e2e1331f2dc79b3452e0bae58422e7d39b1a650
Lucas Garron <code@garron.net>
2021-05-02 04:09:54 -0700

    FREEZE.unindexed

--- src/cubing/puzzles/async/async-pg3d.ts
+++ src/cubing/puzzles/async/async-pg3d.ts
@@ -1,6 +1,4 @@
-import type { KPuzzleDefinition } from "../../kpuzzle";
 import type { PuzzleGeometry } from "../../puzzle-geometry";
-import type { PuzzleLoader } from "../PuzzleLoader";
 
 // TODO: modify this to handle TwistyPlayer options
 export async function asyncGetPuzzleGeometry(
@@ -15,56 +13,4 @@ export async function asyncGetPuzzleGeometry(
     "rotations",
     "true",
   ]);
-}
-
-// TODO: can we cache the puzzleGeometry to avoid duplicate calls, without
-// wasting memory? Maybe just save the latest one for successive calls about the
-// same puzzle?
-export async function asyncGetDef(
-  puzzleName: string,
-): Promise<KPuzzleDefinition> {
-  return (await asyncGetPuzzleGeometry(puzzleName)).writekpuzzle(true);
-}
-
-export function genericPGPuzzleLoader(
-  id: string,
-  fullName: string,
-  info?: {
-    inventedBy?: string[];
-    inventionYear?: number;
-  },
-): PuzzleLoader {
-  const puzzleLoader: PuzzleLoader = {
-    id: id,
-    fullName: fullName,
-    def: async () => {
-      return asyncGetDef(id);
-    },
-    svg: async () => {
-      const pg = await asyncGetPuzzleGeometry(id);
-      return pg.generatesvg();
-    },
-    pg: async () => {
-      return asyncGetPuzzleGeometry(id);
-    },
-  };
-  if (info?.inventedBy) {
-    puzzleLoader.inventedBy = info.inventedBy;
-  }
-  if (info?.inventionYear) {
-    puzzleLoader.inventionYear = info.inventionYear;
-  }
-  return puzzleLoader;
-}
-
-export function cubePGPuzzleLoader(
-  id: string,
-  fullName: string,
-  info?: {
-    inventedBy?: string[];
-    inventionYear?: number;
-  },
-): PuzzleLoader {
-  const puzzleLoader: PuzzleLoader = genericPGPuzzleLoader(id, fullName, info);
-  return puzzleLoader;
 }
