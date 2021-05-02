d039f778bd8e64cbb12062757ac037a159772820
Lucas Garron <code@garron.net>
2021-05-01 21:41:32 -0700

    FREEZE.unindexed

--- src/demo/alg.cubing.net/app.ts
+++ src/demo/alg.cubing.net/app.ts
@@ -1,18 +1,13 @@
-import { Vector3 } from "three";
 import { Alg } from "../../cubing/alg";
 import { puzzles } from "../../cubing/puzzles";
 import {
   TwistyPlayer,
 } from "../../cubing/twisty";
-import { findOrCreateChild, findOrCreateChildWithClass } from "./dom";
 
 console.log(
-  Vector3,
   Alg,
   puzzles,
-  TwistyPlayer,
-  findOrCreateChild,
-  findOrCreateChildWithClass
+  TwistyPlayer
 )
 
 export class App {
