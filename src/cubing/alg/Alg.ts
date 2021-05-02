2e567b5cfad8c531d5cb0663b77d13e67b1f0d14
Lucas Garron <code@garron.net>
2021-05-02 03:46:54 -0700

    FREEZE.unindexed

--- src/cubing/alg/Alg.ts
+++ src/cubing/alg/Alg.ts
@@ -1,12 +1,7 @@
-import { AlgCommon, Comparable } from "./common";
-import { experimentalIs, experimentalIsUnit } from "./is";
+import { AlgCommon } from "./common";
+import { experimentalIsUnit } from "./is";
 
-export type FlexibleAlgSource = string | Iterable<Unit> | Alg;
+console.log(experimentalIsUnit)
 
 export class Alg extends AlgCommon<Alg> {
-  #units: Iterable<Unit>; // TODO: freeze?
-  constructor(alg?: FlexibleAlgSource) {
-    super();
-    console.log(experimentalIsUnit)
-  }
 }
