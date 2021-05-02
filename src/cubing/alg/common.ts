a2b9d4c5c97f880cfe27b1b0c99c19eb05177d10
Lucas Garron <code@garron.net>
2021-05-02 03:50:14 -0700

    FREEZE.unindexed

--- src/cubing/alg/common.ts
+++ src/cubing/alg/common.ts
@@ -2,38 +2,6 @@ import type { Alg } from "./Alg";
 import type { IterationDirection } from "./iteration";
 import type { LeafUnit, Unit } from "./units/Unit";
 
-let writeAlgDebugField = false;
-export function setAlgDebugField(debug: boolean): void {
-  writeAlgDebugField = debug;
-}
-
-export abstract class Comparable {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  is(c: any): boolean {
-    return this instanceof c;
-  }
-
-  abstract isIdentical(other: Comparable): boolean;
-}
-
-export interface Repeatable extends Comparable {
-  experimentalExpand(
-    iterDir?: IterationDirection,
-    depth?: number,
-  ): Generator<LeafUnit>;
-}
-
 // Common to algs or units
-export abstract class AlgCommon<T extends Alg | Unit>
-  extends Comparable
-  implements Repeatable {
-  constructor() {
-    super()
-  }
-
-  abstract toString(): string;
-
-  abstract invert(): T;
-
-  abstract experimentalExpand(iterDir: IterationDirection): Generator<LeafUnit>;
+export abstract class AlgCommon<T extends Alg | Unit> {
 }
