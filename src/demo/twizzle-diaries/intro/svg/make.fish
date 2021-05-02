d812063cc22f1effc3a224ad8c59bbdc8f9e8a2e
Lucas Garron <code@garron.net>
2021-05-01 21:12:47 -0700

    Remove as much demo code as possible while keeping the repro.

--- src/demo/twizzle-diaries/intro/svg/make.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-echo "export const svgFiles = {" > index.ts
-for i in *.svg
-  echo -n "  \"$i\": `" >> index.ts
-    cat $i >> index.ts
-  echo "`," >> index.ts
-end
-echo "};" >> index.ts
--- src/demo/vr/vr-puzzle.ts
+++
@@ -1,6 +0,0 @@
-import type { Group } from "three";
-
-export interface VRPuzzle {
-  group: Group;
-  update(): void;
-}
