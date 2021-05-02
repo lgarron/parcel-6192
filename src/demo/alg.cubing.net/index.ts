bc2acbf4da806255dfcebe3b79accb49fda8b9a4
Lucas Garron <code@garron.net>
2021-05-01 21:14:24 -0700

    Remove most `alg.cubing.net` code.

--- src/demo/alg.cubing.net/index.ts
+++ src/demo/alg.cubing.net/index.ts
@@ -4,31 +4,33 @@ import { getURLParam } from "./url-params";
 import { useNewFaceNames } from "../../cubing/puzzle-geometry";
 import { Alg } from "../../cubing/alg";
 
-useNewFaceNames(true);
+console.log(App, getURLParam, useNewFaceNames, Alg)
 
-window.addEventListener("load", () => {
-  if (!getURLParam("debug-js")) {
-    return;
-  }
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const appElement = document.querySelector("app")!;
-  let experimentalSetupAlg: Alg;
-  try {
-    experimentalSetupAlg = getURLParam("experimental-setup-alg");
-  } catch (e) {
-    experimentalSetupAlg = new Alg();
-  }
-  let alg: Alg;
-  try {
-    alg = getURLParam("alg");
-  } catch (e) {
-    alg = new Alg([]);
-  }
-  (window as any).app = new App(appElement, {
-    puzzleName: getURLParam("puzzle"),
-    experimentalSetupAlg,
-    alg,
-    experimentalSetupAnchor: getURLParam("experimental-setup-anchor"),
-    experimentalStickering: getURLParam("experimental-stickering"),
-  });
-});
+// useNewFaceNames(true);
+
+// window.addEventListener("load", () => {
+//   if (!getURLParam("debug-js")) {
+//     return;
+//   }
+//   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+//   const appElement = document.querySelector("app")!;
+//   let experimentalSetupAlg: Alg;
+//   try {
+//     experimentalSetupAlg = getURLParam("experimental-setup-alg");
+//   } catch (e) {
+//     experimentalSetupAlg = new Alg();
+//   }
+//   let alg: Alg;
+//   try {
+//     alg = getURLParam("alg");
+//   } catch (e) {
+//     alg = new Alg([]);
+//   }
+//   (window as any).app = new App(appElement, {
+//     puzzleName: getURLParam("puzzle"),
+//     experimentalSetupAlg,
+//     alg,
+//     experimentalSetupAnchor: getURLParam("experimental-setup-anchor"),
+//     experimentalStickering: getURLParam("experimental-stickering"),
+//   });
+// });
