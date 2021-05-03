7aad76a4d6d348c4313e80e5057ac4bed8ce34d1
Lucas Garron <code@garron.net>
2021-05-02 18:31:59 -0700

    FREEZE.indexed

--- src/cubing/puzzle-geometry/NotationMapper.ts
+++ src/cubing/puzzle-geometry/NotationMapper.ts
@@ -22,49 +22,10 @@ export class FaceRenamingMapper implements NotationMapper {
     public externalNames: FaceNameSwizzler,
   ) {}
 
-  // TODO:  consider putting a cache in front of this
-  public convertString(
-    grip: string,
-    a: FaceNameSwizzler,
-    b: FaceNameSwizzler,
-  ): string {
-    let suffix = "";
-    if ((grip.endsWith("v") || grip.endsWith("v")) && grip <= "_") {
-      suffix = grip.slice(grip.length - 1);
-      grip = grip.slice(0, grip.length - 1);
-    }
-    const upper = grip.toUpperCase();
-    let isLowerCase = false;
-    if (grip !== upper) {
-      isLowerCase = true;
-      grip = upper;
-    }
-    grip = b.joinByFaceIndices(a.splitByFaceNames(grip));
-    if (isLowerCase) {
-      grip = grip.toLowerCase();
-    }
-    return grip + suffix;
-  }
-
-  public convert(move: Move, a: FaceNameSwizzler, b: FaceNameSwizzler): Move {
-    const grip = move.family;
-    const ngrip = this.convertString(grip, a, b);
-    if (grip === ngrip) {
-      return move;
-    } else {
-      return new Move(
-        new QuantumMove(ngrip, move.innerLayer, move.outerLayer),
-        move.effectiveAmount,
+  public convert(): Move {
+    return new Move(
+        new QuantumMove("R"),
+        2,
       );
-    }
-  }
-
-  public notationToInternal(move: Move): Move {
-    const r = this.convert(move, this.externalNames, this.internalNames);
-    return r;
-  }
-
-  public notationToExternal(move: Move): Move {
-    return this.convert(move, this.internalNames, this.externalNames);
   }
 }
