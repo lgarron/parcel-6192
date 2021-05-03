0adcbaf8cec5c86ee76688277a9b49072470acb7
Lucas Garron <code@garron.net>
2021-05-02 19:25:09 -0700

    FREEZE.unindexed

--- src/cubing/puzzles/async/async-pg3d.ts
+++ src/cubing/puzzles/async/async-pg3d.ts
@@ -1,14 +1,5 @@
 // TODO: modify this to handle TwistyPlayer options
 export async function asyncGetPuzzleGeometry(
-  puzzleName: string,
-): Promise<PuzzleGeometry> {
-  const puzzleGeometry = await import("../../puzzle-geometry");
-  return puzzleGeometry.getPuzzleGeometryByName(puzzleName, [
-    "allmoves",
-    "true",
-    "orientcenters",
-    "true",
-    "rotations",
-    "true",
-  ]);
+): Promise<any> {
+  console.log(await import("../../puzzle-geometry"));
 }
