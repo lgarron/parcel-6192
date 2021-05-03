1f828bed5318359a214ff03c20fe16d56cd35716
Lucas Garron <code@garron.net>
2021-05-02 19:30:57 -0700

    FREEZE.unindexed

--- src/cubing/puzzle-geometry/index.ts
+++ src/cubing/puzzle-geometry/index.ts
@@ -7,20 +7,9 @@ import { Move } from "../alg";
 
 class FaceNameSwizzler {
   public prefixFree: boolean = true;
-  public gripnames: string[] = [];
-  constructor(public facenames: string[], gripnames_arg?: string[]) {
-    if (gripnames_arg) {
-      this.gripnames = gripnames_arg;
-    }
-    for (let i = 0; this.prefixFree && i < facenames.length; i++) {
-      for (let j = 0; this.prefixFree && j < facenames.length; j++) {
-        if (i !== j && facenames[i].startsWith(facenames[j])) {
-          this.prefixFree = false;
-        }
-      }
-    }
+  constructor() {
+    console.log("foo")
   }
-
 }
 
 console.log(FaceNameSwizzler, new Move("R2"))
