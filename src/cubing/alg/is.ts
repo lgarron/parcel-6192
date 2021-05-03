bde801c2d9bd980509e5c3eca88793c610a31b6b
Lucas Garron <code@garron.net>
2021-05-02 20:21:54 -0700

    FREEZE.unindexed

--- src/cubing/alg/is.ts
+++ src/cubing/alg/is.ts
@@ -1,20 +1,8 @@
-import type { Alg } from "./Alg";
 import {
   Commutator,
 } from "./units";
 
-export function experimentalIs(
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  v: any,
-  c:
-    | typeof Alg
-    | typeof Commutator
-): boolean {
-  return v instanceof c;
-}
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function experimentalIsUnit(v: any): boolean {
-  return (
-    experimentalIs(v, Commutator)
-  );
+  return v instanceof Commutator
 }
