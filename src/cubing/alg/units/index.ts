7e91a5367390844f3b08237b204636ab750d1c42
Lucas Garron <code@garron.net>
2021-05-02 19:41:04 -0700

    FREEZE.unindexed

--- src/cubing/alg/units/index.ts
+++ src/cubing/alg/units/index.ts
@@ -1,5 +1,2 @@
-export { Grouping } from "./containers/Grouping";
-export { LineComment } from "./leaves/LineComment";
 export { Commutator } from "./containers/Commutator";
-export { Conjugate } from "./containers/Conjugate";
 export { Pause } from "./leaves/Pause";
