ff2ed144d4e818c2992b5aea65019e6bd6243bb5
Lucas Garron <code@garron.net>
2021-05-02 19:26:37 -0700

    FREEZE.unindexed

--- src/cubing/puzzle-geometry/index.ts
+++ src/cubing/puzzle-geometry/index.ts
@@ -1,5 +1,27 @@
-import "./PuzzleGeometry";
+/* tslint:disable no-bitwise */
+/* tslint:disable prefer-for-of */ // TODO
+/* tslint:disable only-arrow-functions */ // TODO
+/* tslint:disable typedef */ // TODO
 
-export function getPuzzleGeometryByName(): number {
-  return 4;
+import { Move } from "../alg";
+
+class FaceNameSwizzler {
+  public prefixFree: boolean = true;
+  public gripnames: string[] = [];
+  constructor(public facenames: string[], gripnames_arg?: string[]) {
+    if (gripnames_arg) {
+      this.gripnames = gripnames_arg;
+    }
+    for (let i = 0; this.prefixFree && i < facenames.length; i++) {
+      for (let j = 0; this.prefixFree && j < facenames.length; j++) {
+        if (i !== j && facenames[i].startsWith(facenames[j])) {
+          this.prefixFree = false;
+        }
+      }
+    }
+  }
+
 }
+
+
+console.log(FaceNameSwizzler, new Move("R2"))
--- src/cubing/puzzles/index.ts
+++ src/cubing/puzzles/index.ts
@@ -1,7 +1,6 @@
 
 export const cube3x3x3 = {
   def: async () => {
-    // return await import("./3x3x3.kpuzzle.json");
     return {};
   },
   pg: async () => {
