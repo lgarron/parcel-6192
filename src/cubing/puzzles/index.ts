4ef818719794a57a0101bd30fc2d3a394a371a20
Lucas Garron <code@garron.net>
2021-05-02 20:29:47 -0700

    FREEZE.unindexed

--- src/cubing/puzzles/index.ts
+++ src/cubing/puzzles/index.ts
@@ -1,9 +1,4 @@
 
-export const cube3x3x3 = {
-  def: async () => {
-    return {};
-  },
-  pg: async () => {
-    console.log(await import("../puzzle-geometry"));
-  },
-};
+(async () => {
+  console.log(await import("../puzzle-geometry"));
+})();
