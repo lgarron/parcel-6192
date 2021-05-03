5fab341b1e4b8d95601078ba97263a08b9795b06
Lucas Garron <code@garron.net>
2021-05-02 20:03:32 -0700

    FREEZE.unindexed

--- src/cubing/puzzle-geometry/index.ts
+++ src/cubing/puzzle-geometry/index.ts
@@ -1,8 +1,3 @@
-/* tslint:disable no-bitwise */
-/* tslint:disable prefer-for-of */ // TODO
-/* tslint:disable only-arrow-functions */ // TODO
-/* tslint:disable typedef */ // TODO
-
 import { Move } from "../alg";
 
 class FaceNameSwizzler {
--- src/demo/robot/logs/index.ts
+++ src/demo/robot/logs/index.ts
@@ -1,6 +1,3 @@
-// Stub file for testing.
-// Feel free to add code here if you need a quick place to run some code, but avoid committing any changes.
-
 import { Alg } from "../../../cubing/alg";
 
 console.log(Alg)
