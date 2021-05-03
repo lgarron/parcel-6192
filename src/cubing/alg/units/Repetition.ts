d85f61266cf0321817e166d692b17b43d8214260
Lucas Garron <code@garron.net>
2021-05-02 20:11:53 -0700

    FREEZE.indexed

--- src/cubing/alg/units/Repetition.ts
+++ src/cubing/alg/units/Repetition.ts
@@ -1,39 +1,3 @@
 
 export class Repetition<Q extends Repeatable> {
-  readonly quantum: Q;
-  readonly absAmount: number | null = null;
-  readonly prime: boolean = false;
-
-  constructor(quantum: Q, repetitionInfo?: RepetitionInfo) {
-    this.quantum = quantum;
-    if (typeof repetitionInfo === "undefined" || repetitionInfo === null) {
-      // nothing
-    } else if (typeof repetitionInfo === "number") {
-      this.absAmount =
-        repetitionInfo === null ? null : Math.abs(repetitionInfo);
-      this.prime = repetitionInfo === null ? false : repetitionInfo < 0;
-      return;
-    } else if (repetitionInfo instanceof Array) {
-      this.absAmount = repetitionInfo[0] === null ? null : repetitionInfo[0];
-      this.prime = repetitionInfo[1];
-    } else {
-      throw new Error("invalid repetition");
-    }
-
-    if (this.absAmount !== null) {
-      if (
-        !Number.isInteger(this.absAmount) ||
-        this.absAmount! < 0 ||
-        this.absAmount > MAX_INT
-      ) {
-        throw new Error(
-          `Unit amount absolute value must be a non-negative integer no larger than ${MAX_INT_DESCRIPTION}.`,
-        );
-      }
-    }
-
-    if (this.prime !== false && this.prime !== true) {
-      throw new Error("Invalid prime boolean.");
-    }
-  }
 }
