2a65d9c45c38b1d1a1c64a2480cab1a85ae0efa1
Lucas Garron <code@garron.net>
2021-05-02 20:26:21 -0700

    FREEZE.unindexed

--- src/cubing/alg/index.ts
+++ src/cubing/alg/index.ts
@@ -6,8 +6,6 @@ export class Move {
 class Repetition {
 }
 
-export class QuantumCommutator {
-}
 
 export class Commutator {
   readonly #repetition: Repetition = null;
@@ -15,9 +13,6 @@ export class Commutator {
   constructor(
   ) {
     this.#repetition = new Repetition(
-      new QuantumCommutator(
-      ), // TODO
-      repetitionInfo,
     );
   }
 
