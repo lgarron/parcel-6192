d186dfe135ed8a21854aec7c05f98cc05c0d1aae
Lucas Garron <code@garron.net>
2021-05-02 20:11:01 -0700

    FREEZE.unindexed

--- src/cubing/alg/units/leaves/Pause.ts
+++ src/cubing/alg/units/leaves/Pause.ts
@@ -1,24 +1,3 @@
-import { AlgCommon, Comparable } from "../../common";
-import { IterationDirection } from "../../iteration";
-import type { LeafUnit } from "../Unit";
+export class Pause {
 
-export class Pause extends AlgCommon<Pause> {
-  toString(): string {
-    return `.`;
-  }
-
-  isIdentical(other: Comparable): boolean {
-    return other.is(Pause);
-  }
-
-  invert(): Pause {
-    return this;
-  }
-
-  *experimentalExpand(
-    _iterDir: IterationDirection = IterationDirection.Forwards,
-    _depth: number = Infinity,
-  ): Generator<LeafUnit> {
-    yield this;
-  }
 }
