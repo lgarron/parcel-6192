fa3a09cad73f0fa4f754286334f1f29b5d7a0977
Lucas Garron <code@garron.net>
2021-05-02 19:37:03 -0700

    FREEZE.unindexed

--- src/cubing/alg/is.ts
+++ src/cubing/alg/is.ts
@@ -1,11 +1,6 @@
 import type { Alg } from "./Alg";
 import {
   Commutator,
-  Conjugate,
-  Grouping,
-  LineComment,
-  Move,
-  Newline,
   Pause,
 } from "./units";
 
@@ -14,12 +9,7 @@ export function experimentalIs(
   v: any,
   c:
     | typeof Alg
-    | typeof Grouping
-    | typeof LineComment
     | typeof Commutator
-    | typeof Conjugate
-    | typeof Move
-    | typeof Newline
     | typeof Pause,
 ): boolean {
   return v instanceof c;
@@ -27,12 +17,7 @@ export function experimentalIs(
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function experimentalIsUnit(v: any): boolean {
   return (
-    experimentalIs(v, Grouping) ||
-    experimentalIs(v, LineComment) ||
     experimentalIs(v, Commutator) ||
-    experimentalIs(v, Conjugate) ||
-    experimentalIs(v, Move) ||
-    experimentalIs(v, Newline) ||
     experimentalIs(v, Pause)
   );
 }
